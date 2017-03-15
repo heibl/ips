a161be92d919c144e0ea8e1cec19ba023f66ef25
Christoph Heibl <heibsta@gmx.net>
2017-03-15 14:15:29 +0100

    mafft deletes guide tree after running

--- R/mafft.R
+++ R/mafft.R
@@ -1,59 +1,57 @@
 ## This code is part of the ips package
-## © C. Heibl 2014 (last update 2016-11-22)
+## © C. Heibl 2014 (last update 2017-03-15)
 
 #' @export
 
-mafft <- function(x, y, add, method = "auto", maxiterate = 0, 
-                  op = 1.53, ep = 0.0, gt, options, 
+mafft <- function(x, y, add, method = "auto", maxiterate = 0,
+                  op = 1.53, ep = 0.0, gt, options,
                   thread = -1, path, quiet){
-  
+
   ## CHECKS and DEFINITIONS
   ## ----------------------
-  if ( !inherits(x, "DNAbin") ) stop("'x' is not of class 'DNAbin'")
+  if (!inherits(x, "DNAbin")) stop("'x' is not of class 'DNAbin'")
   os <- .Platform$OS
-  if ( missing(quiet) ) quiet <- TRUE
+  if (missing(quiet)) quiet <- TRUE
   qut <- ifelse(quiet, " --quiet ", " ")
-  if ( missing(path) ) path <- "/usr/local/bin/mafft"
+  if (missing(path)) path <- "/usr/local/bin/mafft"
   maxiterate <- match.arg(as.character(maxiterate), c("0", "2", "1000"))
-  
+
   ## temporary input/output files
   ## ----------------------------
-#   fns <- c("mafftin.fas", "mafftpro.fas", "mafftout.fas")
   fns <- vector(length = 3)
-  for ( i in seq_along(fns) ) 
+  for (i in seq_along(fns))
     fns[i] <- tempfile(pattern = "mafft", tmpdir = tempdir(), fileext = ".fas")
   unlink(fns[file.exists(fns)])
-  method <- match.arg(method, c("auto", "localpair", "globalpair", 
+  method <- match.arg(method, c("auto", "localpair", "globalpair",
                                 "genafpair", "parttree",
                                 "retree 1", "retree 2"))
-  
+
   ## guide tree
   ## ----------
-  if ( missing(gt) ){
+  if (missing(gt)){
     gt <- ""
   } else {
-    if ( !inherits(gt, "phylo") )                                          
+    if (!inherits(gt, "phylo"))
       stop("object \"gt\" is not of class \"phylo\"")
-    if ( !all(names(x) %in% gt$tip.label) )
+    if (!all(names(x) %in% gt$tip.label))
       stop("guide tree does not match sequence names")
     gt$tip.label <- match(names(x), gt$tip.label)
-    if ( !is.binary.tree(gt) )
+    if (!is.binary.tree(gt))
       gt <- multi2di(gt)
-    if ( is.null(gt$edge.length) )
+    if (is.null(gt$edge.length))
       gt$edge.length <- rep(1, nrow(gt$edge))
     phylo2mafft(gt)
     gt <- " --treein tree.mafft "
   }
 
-  
   ## multithreading
   ## --------------
   thread <- paste("--thread", thread)
   thread <- paste(rep(" ", 2), collapse = thread)
-  
+
   ## additional arguments specified by the user
   ## ------------------------------------------
-  if ( missing(options) ){
+  if (missing(options)){
     options <- " "
   } else {
     options <- match.arg(options, c("--adjustdirection",
@@ -61,39 +59,38 @@ mafft <- function(x, y, add, method = "auto", maxiterate = 0,
     options <- paste(options, collapse = " ")
     options <- paste(rep(" ", 2), collapse = options)
   }
-  
-  ## write input files and prepare call to MAFFT 
+
+  ## write input files and prepare call to MAFFT
   ## -------------------------------------------
-  if ( missing(y) ){
+  if (missing(y)){
     write.fas(x, fns[1])
-    call.mafft <- paste(path, " --", method, " --",   	
-                        "maxiterate ", maxiterate, qut, "--op ", op, 		
+    call.mafft <- paste(path, " --", method, " --",
+                        "maxiterate ", maxiterate, qut, "--op ", op,
                         " --ep ", ep, gt, options, thread,
                         fns[1], " > ", fns[3], sep = "")
-  }
-	else {
-	  if ( !inherits(y, "DNAbin") ) stop("'y' is not of class 'DNAbin'")
-    if ( missing(add) ) add <- "addprofile"
+  } else {
+	  if (!inherits(y, "DNAbin")) stop("'y' is not of class 'DNAbin'")
+    if (missing(add)) add <- "addprofile"
     add <- match.arg(add, c("add", "addprofile"))
     add <- paste("--", add, sep = "")
 	  write.fas(x, fns[1])
 	  write.fas(y, fns[2])
 	  call.mafft <- paste(path, qut, add, fns[2], fns[1], ">", fns[3])
 	}
-  if ( !quiet ) message(call.mafft)
-  
+  if (!quiet) message(call.mafft)
+
   ## execute MAFFT on UNIX
   ## ---------------------
-  if ( os == "unix" ){
+  if (os == "unix"){
     system(call.mafft, intern = FALSE, ignore.stdout = FALSE)
     res <- length(scan(fns[3], what = "c", quiet = TRUE))
-    if ( res != 0 ) res <- read.fas(fns[3])
-  }
+    if (res != 0) res <- read.fas(fns[3])
+
   ## execute MAFFT on WINDOWS
   ## ------------------------
-  else {
+  } else {
     res <- system(call.mafft, intern = TRUE, ignore.stderr = FALSE)
-    if ( length(grep("error|ERROR", res)) > 0 ){
+    if (length(grep("error|ERROR", res))){
       res <- 0
     }
     else {
@@ -101,6 +98,7 @@ mafft <- function(x, y, add, method = "auto", maxiterate = 0,
     }
   }
   unlink(fns[file.exists(fns)])
+  unlink("tree.mafft")
   return(res)
 }
 
