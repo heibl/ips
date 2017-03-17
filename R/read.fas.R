102e6155e68bb4ac5c84ccc37759f2db365598d8
Christoph Heibl <heibsta@gmx.net>
2017-03-17 11:24:28 +0100

    read.fas modified for AA, but will faile with unaligned AA sequences. Reason: AAbin does not support lists.

--- R/read.fas.R
+++ R/read.fas.R
@@ -1,22 +1,24 @@
 ## This code is part of the ips package
-## © C. Heibl 2014 (last update 2016-11-07)
+## © C. Heibl 2014 (last update 2017-03-17)
 
 #' @export
- 
+
 read.fas <- function(x, text){	
-	
-  if ( !missing(text) ){
+  
+  if (!missing(text)){
     x <- text
   } else {
     x <- scan(x, what = character(), quiet = TRUE)
   }
   
+  if (!length(x)) stop("file is empty")
   
-  if ( length(x) == 0 ) stop("file is empty")
-
   start <- grep("^ {0,}>", x)
+  
+  ## parse taxon names
+  ## -----------------
   h <- unlist(strsplit(x[start][1], ""))
-  if ( length(h) == 1 ){
+  if (length(h) == 1){
     taxnames <- x[start + 1]
     space <- 2
   } else {
@@ -25,22 +27,32 @@ read.fas <- function(x, text){
     space <- 1
   }
   ntax <- length(taxnames)
-    
+  
+  ## parse characters
+  ## ----------------
   start <- c(start, length(x) + 1)
   obj <- vector("list", ntax)
- for ( i in 1:ntax ) 
+  for (i in 1:ntax) 
     obj[[i]] <- unlist(strsplit(gsub(" ", "", x[(start[i] + space):(start[i + 1] - 1)]), NULL))
   names(obj) <- taxnames
   
-#   if ( !all(c("t", "c", "g", "a") %in% tolower(unlist(obj))) ){
-#     ## character data
-#     obj <- do.call(rbind, obj)
-#     obj <- as.data.frame(obj, stringsAsFactors = FALSE)
-#   } else { ## DNA
+  dna_string <- c("n", "?", "-", "r", "y", "s", "w", "k", "m", "b", "d", "h", "v")
+  if (all(tolower(unlist(obj))  %in%  dna_string)){
+    ## DNA sequences
     obj <- lapply(obj, tolower)
     obj <- as.DNAbin(obj)
-    if ( length(unique(sapply(obj, length))) == 1 )
-      obj <- as.matrix(obj)
-#   }
+  } else {
+    ## AA sequences
+    obj <- as.AAbin(obj)
+    
+    ## CH [2017-03-17]
+    ## Hier gehts im Moment nicht weiter, da AAbin keine Listen unterstützt.
+    ## phangorn::read.aa gibt eine Fehlermeldung:
+    ## Fehler in phangorn::read.aa(x) : 
+    ##   the first line of the file must contain the dimensions of the data
+    ## Es wäre wohl sinnvoll die AAbin Klasse zu erweitern
+  }
+  if (length(unique(sapply(obj, length))) == 1)
+    obj <- as.matrix(obj)
   return(obj)
 }
