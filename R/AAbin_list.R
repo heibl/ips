6a2ceae383a8d1b70b5c3e42d4072f4ea06cd387
FranzKrah <f.krah@mailbox.org>
2017-03-22 09:37:05 +0100

    ape extension AAbin

--- R/AAbin_list.R
+++ R/AAbin_list.R
@@ -1,4 +1,4 @@
-## Franz Krah [2017-03-21]
+## Franz Krah [2017-03-22]
 ## These are simple add-on functions to R package ape
 ## Ape currently does not support lists and bit-level coding scheme
 ## for amino acid sequences
@@ -33,3 +33,22 @@ as.AAbin.list <- function(x, ...)
   class(obj) <- "AAbin"
   obj
 }
+
+as.matrix.AAbin <- function(x, ...)
+{
+  if (is.matrix(x)) return(x)
+  if (is.vector(x)) {
+    dim(x) <- c(1, length(x))
+    return(x)
+  }
+  s <- unique(lengths(x, use.names = FALSE))
+  if (length(s) != 1)
+    stop("AA sequences in list not of the same length.")
+  n <- length(x)
+  y <- matrix(raw(), n, s)
+  for (i in seq_len(n)) y[i, ] <- x[[i]]
+  rownames(y) <- names(x)
+  class(y) <- "AAbin"
+  y
+}
+
